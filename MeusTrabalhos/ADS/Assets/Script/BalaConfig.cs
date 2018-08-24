1c78e492f89cd98f4f2c2df4d63dcdefdcbe3448
Rouem <raphaelfelipe999@gmail.com>
2018-08-24 15:56:47 -0300

    enviando os arquivos

---
+++ MeusTrabalhos/ADS/Assets/Script/BalaConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaConfig : MonoBehaviour {
+
+	public float ForcaTiro;
+	public Vector3 ForcaVento;
+	public float DistanciaAlvo;
+	public Transform Alvo;
+	GameObject cam;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		cam = GameObject.Find ("Main Camera");
+		transform.GetComponent<Renderer> ().material.color = Color.red;
+		Alvo.transform.GetComponent<Renderer> ().material.color = Color.blue;
+		rb = transform.GetComponent<Rigidbody> ();
+		rb.AddForce (transform.forward * ForcaTiro, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce(ForcaVento, ForceMode.Force);
+		cam.transform.LookAt (transform);
+	}
+
+	void OnCollisionEnter (Collision col) {
+		DistanciaAlvo = Vector3.Distance (transform.position, Alvo.position);
+		if (DistanciaAlvo < 2)
+			DistanciaAlvo = 0;
+		print ("Distancia do alvo: "+DistanciaAlvo);
+		rb.isKinematic = true;
+		Destroy (gameObject,3);
+	}
+}
---
+++ MeusTrabalhos/ADS/Assets/Script/Canhao.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canhao : MonoBehaviour {
+
+	public Transform alvo;
+	public GameObject bala, ponto;
+	public float angulo, forca, vento;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Rotate ((Input.GetAxis("Mouse X") * 90 * Time.deltaTime), 0, 0);
+
+
+		if (Input.GetMouseButton (0)) {
+			forca += 5 * Time.deltaTime;
+			print (forca);
+		}
+
+		if (Input.GetMouseButtonUp (0)){
+			bala.GetComponent<BalaConfig> ().Alvo = alvo;
+			bala.GetComponent<BalaConfig> ().ForcaTiro = forca;
+			bala.GetComponent<BalaConfig> ().ForcaVento = new Vector3(vento,0,0);
+			Instantiate (bala, ponto.transform.position, transform.rotation);
+			forca = 0;
+			//print("Angulo: "+angulo+"  Força: "+forca);
+	}
+	}
+}
